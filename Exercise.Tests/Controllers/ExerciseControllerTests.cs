a91548e8762a425c5ce3e24438069419270c0c8a
Sasa Vlaisavljevic <sasa.vlaisavljevic@team4rent.hr>
2015-07-27 09:51:02 +0200

    Initial commit

---
+++ Exercise.Tests/Controllers/ExerciseControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Controllers.Tests
+{
+    [TestClass()]
+    public class ExerciseControllerTests
+    {
+        private Models.Exercise1 exercise1;
+        private Models.Exercise3 exercise3;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            exercise1 = new Models.Exercise1();
+            exercise3 = new Models.Exercise3();
+
+        }
+
+        [TestMethod()]
+        public void Exercise1Test()
+        {
+            exercise1.Flight = new int[] { 5 };
+
+            for (var i = 0; i < exercise1.FlightsOfStairs; i++)
+            {
+                exercise1.FlightOfStairs = 4;
+                exercise1.Flight[i] = exercise1.FlightOfStairs;
+            }
+
+            exercise1.StepsPerStride = 3;
+
+            Assert.AreEqual(3, exercise1.GetStrides());
+        }
+
+        [TestMethod()]
+        public void Exercise3TestNullValues()
+        {
+            exercise3.Add_UsingARecursiveAlgorithm_ValuesAreAdded(null, null);
+        }
+
+        [TestMethod()]
+        public void Exercise3TestNullValue()
+        {
+            byte[] testInput = { 0, 0, 1 };
+
+            exercise3.Add_UsingARecursiveAlgorithm_ValuesAreAdded(null, testInput);
+        }
+
+        
+    }
+}
---
+++ Exercise.Tests/Models/Exercise2Tests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Models.Tests
+{
+    [TestClass()]
+    public class Exercise2Tests
+    {
+        [TestMethod()]
+        public void StartTestCaseTest()
+        {
+            var exercise2 = new Models.Exercise2();
+            exercise2.T = 50;
+            exercise2.N = 2;
+
+            Space space = new Space(500, 125, 500);
+
+            List<TestCase> testCaseList = new List<TestCase>();
+            Exercise2.PositionValue = Exercise2.GetValue(space.Height, space.Width, space.Depth);
+
+            int nTemp = 0;
+            for (var i = 1; i <= exercise2.T; i++)
+            {
+                List<Bomb> bombs = new List<Bomb>();
+                nTemp = exercise2.N;
+
+                for (var j = 1; j <= exercise2.N; j++)
+                {
+                    Position myPosition = new Position(200);
+                    bombs.Add(new Bomb(j, 500, 125, 500, myPosition));
+                }
+
+                testCaseList.Add(new TestCase(i, bombs));
+
+                exercise2.N = nTemp;
+            }
+
+            foreach (var testCase in testCaseList)
+            {
+                foreach (var bomb in testCase.bombList)
+                {
+                    exercise2.BombValue = Exercise2.GetValue(bomb.Height, bomb.Width, bomb.Depth);
+
+                    // Find the nearest bomb with the longest distance
+                    Exercise2.Compare(exercise2.BombValue, bomb.Position.Distance, bomb.Number);
+                }
+                Assert.AreEqual(2, Exercise2.BombNumber);
+            }
+        }
+    }
+}
